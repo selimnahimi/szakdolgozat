04842dc3c4bdd4b07b365de7cd0ea766e040590e
HUNcamper <selimnahimi@gmail.com>
2023-01-10 22:05:34 +0100

    Refaktorálás

--- tracer.js
+++ tracer.js
@@ -1,10 +1,10 @@
 const EventEmitter = require('events').EventEmitter;
 const Logger = require('./lib/logger').Logger;
-const util = require('util');
 const Module = require('module');
 
 class Tracer extends EventEmitter {
-    logger;
+    version = 'pre-0.0.1';
+    logger = new Logger(process.stdout, process.stdout.write);
 
     constructor() {
         super();
@@ -12,28 +12,24 @@ class Tracer extends EventEmitter {
     }
 
     inject() {
-        this.logger = new Logger();
-
-        this.log("Hello World!");
-
+        this.log(`-- pigeon ${this.version} --`);
+        this.log('compile felülírása');
         this.alterCompile();
     }
 
-    log() {
-        if (!this.logger) return;
-
-        this.logger.print('tracer: ');
-        this.logger.write.apply(this.logger, arguments);
-    }
-
     alterCompile() {
-        this.log('Module.prototype._compile módosítása');
-
         Module.prototype._compile = function(content, filename) {
             content = Module.wrapper[0] + '\n' + content + Module.wrapper[1];
 
             console.log(content);
         }
+    }
+
+    log() {
+        if (!this.logger) return;
+
+        this.logger.print('pigeon: ');
+        this.logger.write.apply(this.logger, arguments);
     }
 }
 
