262f0a0acfd13183cbdd26b41714c42382f5c532
HUNcamper <selimnahimi@gmail.com>
2023-01-21 14:27:42 +0100

    Hibajavítások a függvény név kinyerésénél

--- test/testproject/index.js
+++ test/testproject/index.js
@@ -1,5 +1,11 @@
 console.log("Hello World!");
 let number1 = 5;
 let number2 = 10;
-let result = number1 + number2;
-console.log(result);
+let result1 = number1 + number2;
+let result2 = testFn(number1, number2);
+console.log(result1);
+console.log(result2);
+
+function testFn(number1, number2) {
+    return number1 + number2;
+}
